d6805ef7c4b3aba802212c008bda2604c77a1a0f
hardy716 <dev.hardy716@gmail.com>
2024-09-22 01:03:22 +0900

    google map test (marker, circle)

--- lib/modules/map/map_v.dart
+++ lib/modules/map/map_v.dart
@@ -18,8 +18,49 @@ class _MapViewState extends ConsumerState<MapView> {
   @override
   Widget build(BuildContext context) {
     late GoogleMapController mapController;
+    Set<Circle> _circles = {
+      Circle(
+        circleId: CircleId('center'),
+        center: LatLng(37.3948, 127.1112),
+        radius: 50,
+        fillColor: Colors.orange.withOpacity(0.2),
+        strokeColor: Colors.orange,
+        strokeWidth: 2,
+      ),
+      Circle(
+        circleId: CircleId('left'),
+        center: LatLng(37.3948, 127.1092),
+        radius: 50,
+        fillColor: Colors.orange.withOpacity(0.2),
+        strokeColor: Colors.orange,
+        strokeWidth: 2,
+      ),
+      Circle(
+        circleId: CircleId('right'),
+        center: LatLng(37.3948, 127.1132),
+        radius: 50,
+        fillColor: Colors.orange.withOpacity(0.2),
+        strokeColor: Colors.orange,
+        strokeWidth: 2,
+      ),
+      Circle(
+        circleId: CircleId('up'),
+        center: LatLng(37.3968, 127.1112),
+        radius: 50,
+        fillColor: Colors.orange.withOpacity(0.2),
+        strokeColor: Colors.orange,
+        strokeWidth: 2,
+      ),Circle(
+        circleId: CircleId('down'),
+        center: LatLng(37.3928, 127.1112),
+        radius: 50,
+        fillColor: Colors.orange.withOpacity(0.2),
+        strokeColor: Colors.orange,
+        strokeWidth: 2,
+      ),
+    };
 
-    final LatLng _center = const LatLng(-33.86, 151.20);
+    final LatLng _center = const LatLng(37.3948, 127.1112);
 
     void _onMapCreated(GoogleMapController controller) {
       mapController = controller;
@@ -29,10 +70,23 @@ class _MapViewState extends ConsumerState<MapView> {
       color: context.colors.primary,
       child: GoogleMap(
         onMapCreated: _onMapCreated,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
         initialCameraPosition: CameraPosition(
           target: _center,
-          zoom: 11.0,
-        ), // CameraPosition
+          zoom: 17.5,
+        ),
+        circles: _circles,
+        markers: {
+          const Marker(
+            markerId: const MarkerId("Center"),
+            position: LatLng(37.3948, 127.1112),
+            infoWindow: InfoWindow(
+              title: "Center",
+              snippet: "판교역",
+            ),
+          ), // Marker
+        },
       ),
     );
   }
