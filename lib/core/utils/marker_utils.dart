8a9a8075d13a0030fddb3bdb1c0d9ae516b8e879
hardy716 <dev.hardy716@gmail.com>
2024-09-22 01:39:28 +0900

    google map test (network-image custom marker) - add package : image - add util : createCustomMarkerFromNetworkImage

---
+++ lib/core/utils/marker_utils.dart
@@ -0,0 +1,42 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:image/image.dart' as img;
+
+class MarkerUtils {
+  static Future<BitmapDescriptor> createCustomMarkerFromNetworkImage(String imageUrl) async {
+    try {
+      final http.Response response = await http.get(Uri.parse(imageUrl));
+
+      if (response.statusCode == 200) {
+        Uint8List uint8list = response.bodyBytes;
+
+        img.Image? image = img.decodeImage(uint8list);
+
+        if (image != null) {
+          img.Image circularImage = img.copyCropCircle(image);
+
+          Uint8List resizedUint8List = Uint8List.fromList(img.encodePng(circularImage));
+
+          ui.Codec codec = await ui.instantiateImageCodec(resizedUint8List);
+          ui.FrameInfo fi = await codec.getNextFrame();
+
+          ByteData? byteData = await fi.image.toByteData(format: ui.ImageByteFormat.png);
+          Uint8List finalImage = byteData!.buffer.asUint8List();
+
+          return BitmapDescriptor.bytes(finalImage, width: 50, height: 50);
+        }
+      }
+    } catch (e) {
+      debugPrint('$e');
+
+      return BitmapDescriptor.defaultMarker;
+    }
+
+    return BitmapDescriptor.defaultMarker;
+  }
+}
+
--- lib/modules/map/map_v.dart
+++ lib/modules/map/map_v.dart
@@ -3,8 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-import '../../core/router/route.dart';
-import '../../core/router/page.dart';
+import '../../core/utils/marker_utils.dart';
 
 class MapView extends ConsumerStatefulWidget {
   const MapView({super.key});
@@ -14,51 +13,100 @@ class MapView extends ConsumerStatefulWidget {
 }
 
 class _MapViewState extends ConsumerState<MapView> {
+  GoogleMapController? mapController;
+  Set<Circle> _circles = {
+    Circle(
+      circleId: CircleId('center'),
+      center: LatLng(37.3948, 127.1112),
+      radius: 50,
+      fillColor: Colors.orange.withOpacity(0.2),
+      strokeColor: Colors.orange,
+      strokeWidth: 2,
+    ),
+    Circle(
+      circleId: CircleId('left'),
+      center: LatLng(37.3948, 127.1092),
+      radius: 50,
+      fillColor: Colors.orange.withOpacity(0.2),
+      strokeColor: Colors.orange,
+      strokeWidth: 2,
+    ),
+    Circle(
+      circleId: CircleId('right'),
+      center: LatLng(37.3948, 127.1132),
+      radius: 50,
+      fillColor: Colors.orange.withOpacity(0.2),
+      strokeColor: Colors.orange,
+      strokeWidth: 2,
+    ),
+    Circle(
+      circleId: CircleId('up'),
+      center: LatLng(37.3968, 127.1112),
+      radius: 50,
+      fillColor: Colors.orange.withOpacity(0.2),
+      strokeColor: Colors.orange,
+      strokeWidth: 2,
+    ),Circle(
+      circleId: CircleId('down'),
+      center: LatLng(37.3928, 127.1112),
+      radius: 50,
+      fillColor: Colors.orange.withOpacity(0.2),
+      strokeColor: Colors.orange,
+      strokeWidth: 2,
+    ),
+  };
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _addMarker();
+  }
+
+  Future<void> _addMarker() async {
+    BitmapDescriptor customMarker = await MarkerUtils.createCustomMarkerFromNetworkImage(
+        'https://lh3.googleusercontent.com/a/ACg8ocJ3BG_hU5cjETU5ybAU4FpBSNKHenbFzzliXiYsCQuHRX1luHU=s576-c-no');
+
+    setState(() {
+      _markers.addAll(
+        [
+          Marker(
+            markerId: MarkerId('center'),
+            position: LatLng(37.3948, 127.1112),
+            anchor: Offset(0.5, 0.5),
+            icon: customMarker,
+          ),
+          Marker(
+            markerId: MarkerId('left'),
+            position: LatLng(37.3948, 127.1092),
+            anchor: Offset(0.5, 0.5),
+            icon: customMarker,
+          ),
+          Marker(
+            markerId: MarkerId('right'),
+            position: LatLng(37.3948, 127.1132),
+            anchor: Offset(0.5, 0.5),
+            icon: customMarker,
+          ),
+          Marker(
+            markerId: MarkerId('up'),
+            position: LatLng(37.3968, 127.1112),
+            anchor: Offset(0.5, 0.5),
+            icon: customMarker,
+          ),
+          Marker(
+            markerId: MarkerId('down'),
+            position: LatLng(37.3928, 127.1112),
+            anchor: Offset(0.5, 0.5),
+            icon: customMarker,
+          ),
+        ],
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    late GoogleMapController mapController;
-    Set<Circle> _circles = {
-      Circle(
-        circleId: CircleId('center'),
-        center: LatLng(37.3948, 127.1112),
-        radius: 50,
-        fillColor: Colors.orange.withOpacity(0.2),
-        strokeColor: Colors.orange,
-        strokeWidth: 2,
-      ),
-      Circle(
-        circleId: CircleId('left'),
-        center: LatLng(37.3948, 127.1092),
-        radius: 50,
-        fillColor: Colors.orange.withOpacity(0.2),
-        strokeColor: Colors.orange,
-        strokeWidth: 2,
-      ),
-      Circle(
-        circleId: CircleId('right'),
-        center: LatLng(37.3948, 127.1132),
-        radius: 50,
-        fillColor: Colors.orange.withOpacity(0.2),
-        strokeColor: Colors.orange,
-        strokeWidth: 2,
-      ),
-      Circle(
-        circleId: CircleId('up'),
-        center: LatLng(37.3968, 127.1112),
-        radius: 50,
-        fillColor: Colors.orange.withOpacity(0.2),
-        strokeColor: Colors.orange,
-        strokeWidth: 2,
-      ),Circle(
-        circleId: CircleId('down'),
-        center: LatLng(37.3928, 127.1112),
-        radius: 50,
-        fillColor: Colors.orange.withOpacity(0.2),
-        strokeColor: Colors.orange,
-        strokeWidth: 2,
-      ),
-    };
 
     final LatLng _center = const LatLng(37.3948, 127.1112);
 
@@ -74,19 +122,10 @@ class _MapViewState extends ConsumerState<MapView> {
         zoomControlsEnabled: true,
         initialCameraPosition: CameraPosition(
           target: _center,
-          zoom: 17.5,
+          zoom: 17,
         ),
         circles: _circles,
-        markers: {
-          const Marker(
-            markerId: const MarkerId("Center"),
-            position: LatLng(37.3948, 127.1112),
-            infoWindow: InfoWindow(
-              title: "Center",
-              snippet: "판교역",
-            ),
-          ), // Marker
-        },
+        markers: _markers,
       ),
     );
   }
